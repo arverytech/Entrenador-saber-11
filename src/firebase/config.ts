fd51bf55d5383f955c7c5038f3b85650c975bbcd
arverytech <arverytech@gmail.com>
2026-04-13 20:02:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6219214237-b11ad",
+  "appId": "1:855957968625:web:356a482c68aa977c1efb50",
+  "apiKey": "AIzaSyD0yPaFohPwXxMa_MwL2YJypQdsUREZg-0",
+  "authDomain": "studio-6219214237-b11ad.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "855957968625"
+};
